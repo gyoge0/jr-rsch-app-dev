105d3683af2e282433e33b8b950799f5ca9a86ec
Yogesh Thambidurai <gyogesh2015@outlook.com>
2024-02-07 11:45:01 -0500

    feat: add navigation to home screen

--- apps/starter/App.jsx
+++ apps/starter/App.jsx
@@ -3,22 +3,40 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import CuteAnimals from "./src/screens/CuteAnimals";
-import HomeScreen from "./src/screens/Home";
+import Home from "./src/screens/Home";
 import ModifyCounter from "./src/screens/ModifyCounter";
 import Snapchat from "./src/screens/Snapchat";
 
 const Stack = createNativeStackNavigator();
 
-// noinspection JSXUnresolvedComponent
-const App = () => (
-    <NavigationContainer>
-        <Stack.Navigator initialRouteName="Snapchat">
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="CuteAnimals" component={CuteAnimals} />
-            <Stack.Screen name="ModifyCounter" component={ModifyCounter} />
-            <Stack.Screen name="Snapchat" component={Snapchat} />
-        </Stack.Navigator>
-    </NavigationContainer>
-);
+const App = () => {
+    const screens = [
+        { name: "Cute Animals", component: CuteAnimals },
+        { name: "Modify Counter", component: ModifyCounter },
+        { name: "Snapchat", component: Snapchat },
+    ];
+
+    // noinspection JSXUnresolvedComponent
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    initialParams={{
+                        screens: screens.map(screen => screen.name)
+                    }}
+                />
+                {screens.map(({ name, component }) => (
+                    <Stack.Screen
+                        key={name}
+                        name={name}
+                        component={component}
+                    />
+                ))}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
 
 export default App;
--- apps/starter/src/screens/Home.jsx
+++ apps/starter/src/screens/Home.jsx
@@ -1,10 +1,21 @@
 /* eslint-disable no-unused-vars */
 
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Button, View, StyleSheet } from "react-native";
 
-const Home = () => {
-    return <Text style={styles.text}>HomeScreen</Text>;
+const Home = ({ navigation, route }) => {
+    const { screens } = route.params;
+    return (
+        <View>
+            {screens.map((screen) => (
+                <Button
+                    title={screen}
+                    onPress={() => navigation.navigate(screen)}
+                    key={screen}
+                />
+            ))}
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
