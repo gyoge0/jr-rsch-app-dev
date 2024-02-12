824cb2cb5d1ff09ecae5c32a94231fcf446a5c8d
Yogesh Thambidurai <gyogesh2015@outlook.com>
2024-02-12 12:16:30 -0500

    lab: the predictor

--- apps/starter/App.jsx
+++ apps/starter/App.jsx
@@ -6,6 +6,7 @@ import CuteAnimals from "./src/screens/CuteAnimals";
 import Home from "./src/screens/Home";
 import ModifyCounter from "./src/screens/ModifyCounter";
 import NorwayFlag from "./src/screens/NorwayFlag";
+import ThePredictor from "./src/screens/ThePredictor";
 import Snapchat from "./src/screens/Snapchat";
 import SongList from "./src/screens/SongList";
 import SwedenFlag from "./src/screens/SwedenFlag";
@@ -20,6 +21,7 @@ const App = () => {
         { name: "Sweden Flag", component: SwedenFlag },
         { name: "Song List", component: SongList },
         { name: "Norway Flag", component: NorwayFlag },
+        { name: "The Predictor", component: ThePredictor },
     ];
 
     // noinspection JSXUnresolvedComponent
---
+++ apps/starter/src/screens/ThePredictor.jsx
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import {
+    Text,
+    StyleSheet,
+    View,
+    Image,
+    TextInput,
+    Dimensions,
+    Button,
+    ImageBackground,
+} from "react-native";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+const ThePredictor = () => {
+    const [text, setText] = useState("");
+    const [displayText, setDisplayText] = useState("");
+    const [prediction, setPrediction] = useState("");
+    const [predictionList, setPredictionList] = useState([
+        "Yes",
+        "No",
+        "Try again later",
+    ]);
+
+    const setQuestion = (newText) => setText(newText);
+
+    const updateScreen = () => {
+        setDisplayText(text);
+        setText("");
+        const prediction =
+            predictionList[Math.floor(Math.random() * predictionList.length)];
+        setPrediction(prediction);
+    };
+
+    const image = {
+        uri: "https://c1.staticflickr.com/6/5253/5426098861_4aee73fb6d_b.jpg",
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.titleBlock}>
+                <Text style={styles.titleText}>The Predictor </Text>
+            </View>
+            <View style={styles.firstBlock}>
+                <Text style={styles.questionText}>Question: </Text>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="enter your question here"
+                    onChangeText={(newText) => setQuestion(newText)}
+                    defaultValue={text}
+                />
+                <Button
+                    onPress={updateScreen}
+                    title="GET PREDICTION"
+                    color="yellow"
+                />
+            </View>
+            <View style={styles.secondBlock}>
+                <ImageBackground
+                    source={image}
+                    resizeMode="cover"
+                    style={styles.image}
+                >
+                    <Text style={styles.predictionText}>{displayText}</Text>
+                    <Text style={styles.predictionText}> </Text>
+                    <Text style={styles.predictionText}>{prediction}</Text>
+                </ImageBackground>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "yellow",
+        justifyContent: "space-between",
+        flex: 1,
+    },
+    titleBlock: {
+        backgroundColor: "black",
+        flex: 1,
+    },
+    firstBlock: {
+        backgroundColor: "darkblue",
+        flex: 2,
+    },
+    secondBlock: {
+        backgroundColor: "lightgray",
+        flex: 6,
+    },
+    titleText: {
+        color: "white",
+        fontSize: 40,
+        textAlign: "center",
+    },
+    textInput: {
+        backgroundColor: "black",
+        color: "white",
+        height: 40,
+        fontSize: 20,
+    },
+    questionText: {
+        color: "yellow",
+        fontSize: 35,
+        textAlign: "center",
+    },
+    predictionText: {
+        color: "yellow",
+        fontSize: 50,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    image: {
+        justifyContent: "center",
+        flex: 1,
+    },
+});
+
+export default ThePredictor;
